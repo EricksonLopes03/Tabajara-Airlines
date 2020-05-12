4b3bd46c29e30c374d6be4457b18506b03feb207
LUIZ GUSTAVO <luiz50gustavo@gmail.com>
2020-05-12 10:21:53 -0300

    v003

--- Tabajara Airlines/src/principal/GerenciaCliente.java
+++ Tabajara Airlines/src/principal/GerenciaCliente.java
@@ -1,5 +1,72 @@
 package principal;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GerenciaCliente {
 
+	
+	private ArrayList<Cliente> cli;
+	private Scanner sc = new Scanner(System.in);
+	
+	public GerenciaCliente(ArrayList<Cliente> cli) {
+		this.cli = cli;
+
+	}
+
+	
+	
+	public void cadastrar(){
+		
+		int id;
+		String nome, log, numero, bairro, municipio, estado, cep, telefone;
+		boolean idDisponivel = true;
+		
+		do {
+			idDisponivel = true;
+		System.out.println("Digite a identificação do cliente: ");
+		id = sc.nextInt();
+		for(Cliente c : cli) {
+			if(c.getIdentificacao() == id) {
+				System.out.println("Esta identificação já está em uso");
+				idDisponivel = false;
+			}
+		}
+		
+		}while(idDisponivel == false);
+		sc.nextLine();
+		
+		System.out.println("Digite o nome do cliente");
+		nome = sc.nextLine();
+		System.out.println("Digite o logradouro: ");
+		log = sc.nextLine();
+		System.out.println("Digite o número: ");
+		numero = sc.nextLine();
+		System.out.println("Digite o bairro: ");
+		bairro = sc.nextLine();
+		System.out.println("Ditie o município: ");
+		municipio = sc.nextLine();
+		System.out.println("Digite o estado: ");
+		estado = sc.nextLine();
+		System.out.println("Digite o CEP: ");
+		cep = sc.nextLine();
+		System.out.println("Digite o telefone: ");
+		telefone = sc.nextLine();
+		
+		cli.add(new Cliente(id, nome, log, numero, bairro, municipio, estado, cep, telefone));
+		System.out.println("Cliente provavelmente cadastrado com sucesso;");
+		
+		
+	}
+	
+	
+	
+	public void relatorio() {
+		
+		for(Cliente c : cli) {
+			c.imprimir();
+		}
+	}
+	
+	
 }
--- Tabajara Airlines/src/principal/Main.java
+++ Tabajara Airlines/src/principal/Main.java
@@ -1,5 +1,44 @@
 package principal;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+	public static void main(String[] args) {
+	ArrayList<Cliente> clientes = new ArrayList<>();
+	GerenciaCliente gc = new GerenciaCliente(clientes);
+	Scanner sc = new Scanner(System.in);
+	int op, sop;
+	
+	do {
+		System.out.println("1 - Clientes");
+		System.out.println("0 - Encerrar aplicação");
+		op = sc.nextInt();
+		switch(op) {
+		case 1:
+			do {
+				System.out.println("1 - Cadastrar");
+				System.out.println("2 - Alterar");
+				System.out.println("3 - Excluir");
+				System.out.println("4 - Relatório");
+				System.out.println("0 - Voltar");
+				sop = sc.nextInt();
+				switch(sop) {
+				case 1:
+					gc.cadastrar();
+					break;
+				case 2:
+					gc.relatorio();
+					break;
+				}
+				
+			}while(sop != 0);
+		case 0:
+			System.out.println("Encerrado");
+		}
+	}while(op != 0);
+	
+	
+	}
 }
